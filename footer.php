ac8c8eee594b636d71368e4107fe72c562cf9fd0
Sundar Krishnamurthy <sundar_k@hotmail.com>
2017-06-18 15:44:10 -0700

    Create footer.php
    Footer at the bottom of every page

---
+++ footer.php
@@ -0,0 +1,36 @@
+<?php
+// Module Name:  xm website
+// Project:      xm website to track everyday expenses
+//
+// footer.php - display at bottom of every page
+//
+// Cookies:
+//
+// Session Variables:
+//
+// Stored Procedures:
+//
+// JavaScript methods:
+//
+// CSS Labels:
+//     fillerPanel40px, footerContent, footerTable, commonLabelCenter
+//
+// Revisions:
+//     1. Sundar Krishnamurthy          sundar_k@hotmail.com       06/10/2017      Initial file created.
+?>
+
+<div class="fillerPanel40px"/>
+<div class="footerContent">
+  <table class="footerTable">
+    <tbody>
+      <tr>
+        <td class="commonLabelCenter">
+            &nbsp;<br/>
+            &nbsp;Manage your money - track every expense, income, transaction and categorize all your spending.<br/>
+            &nbsp;This site is under construction, please check back later.
+      </td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+<div class="fillerPanel40px"></div>
